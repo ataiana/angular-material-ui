5951d34ea68f9b8839034438faa6db59d224939a
AT <ataiana@gmail.com>
2021-03-27 19:18:51 -0300

    Heroku deploy

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/angular-material-eleven'));
+
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {
+        root: 'dist/angular-material-eleven/'
+    });
+  });
+
+  const port = process.env.PORT || 8080;
+
+  app.listen(port, () => console.log('Listening to port: ' + port) );
