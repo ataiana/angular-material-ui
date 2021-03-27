12d7d9b51c28ca60618a7df8c176ddf3e716d573
AT <ataiana@gmail.com>
2021-03-27 19:41:25 -0300

    updated server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const app = express();
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-app.use(requireHTTPS);
 
-app.use(express.static('./dist/angular-material-eleven'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/dist/angular-material-eleven');
+});
+app.use(express.static( __dirname + '/dist'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {
-        root: 'dist/angular-material-eleven/'
-    });
-  });
 
-  const port = process.env.PORT || 8080;
 
-  app.listen(port, () => console.log('Listening to port: ' + port) );
+  process.env.PORT = process.env.PORT || 8080;
+
+  app.listen(process.env.PORT, () => console.log('Listening to port: ' + process.env.PORT) );
